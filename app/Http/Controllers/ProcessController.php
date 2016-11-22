f05979955e34ae2f81402414c486b9a06ce585ee
fenicesun <fenice.suns@gmail.com>
2016-11-22 17:12:00 +0800

    process/list

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -27,8 +27,25 @@ class ProcessController extends Controller
             $cmd .= ("|" . $filter);
         }
         exec($cmd, $output, $return_val);
+        if ($return_val != 0) {
+            return response()
+                ->json(['code' => -1, 'data' => NULL]);
+        }
+        $header = explode(" ",
+            preg_replace("/\s(?=\s)/","\\1",$output[0]) );
+
+        $process_infos = array();
+        for ($i = 1; $i < count($output); $i++) {
+            $body = explode(" ",
+               preg_replace("/\s(?=\s)/", "\\1", $output[$i]) );
+            $process_item = array();
+            for ($j = 0; $j < count($header); $j++) {
+                $process_item[$header[$j]] = $body[$j];
+            }
+            $process_infos[] = $process_item;
+        }
         return response()
-            ->json(['code' => $return_val, 'data' => $output]);
+            ->json(['code' => 0, 'data' => $process_infos]);
     }
 
 
